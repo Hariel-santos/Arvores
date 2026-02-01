1aeb89adc74cc00359fd6cc6e4bc8df7cf272f53
Hariel Santos <harielp.santos1@gmail.com>
2026-02-01 06:40:57 -0400

    arvorvoreEmC

--- c.c
+++ c.c
@@ -1,28 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No {
+    int valor;
+    struct No *esq;
+    struct No *dir;
+} No;
+
+/* Funções */
+No* criarNo(int valor);
+No* inserir(No* raiz, int valor);
+void emOrdem(No* raiz);
+void liberar(No* raiz);
+
+No* criarNo(int valor) {
+    No* novo = (No*) malloc(sizeof(No));
+    novo->valor = valor;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    return novo;
+}
+
+No* inserir(No* raiz, int valor) {
+    if (raiz == NULL)
+        return criarNo(valor);
+
+    if (valor < raiz->valor)
+        raiz->esq = inserir(raiz->esq, valor);
+    else if (valor > raiz->valor)
+        raiz->dir = inserir(raiz->dir, valor);
+
+    return raiz;
+}
+
+void emOrdem(No* raiz) {
+    if (raiz != NULL) {
+        emOrdem(raiz->esq);
+        printf("%d ", raiz->valor);
+        emOrdem(raiz->dir);
+    }
+}
+
+void liberar(No* raiz) {
+    if (raiz != NULL) {
+        liberar(raiz->esq);
+        liberar(raiz->dir);
+        free(raiz);
+    }
+}
 
 int main() {
-    int base[100];
-    int tamanho = 0;   
-    int numAdd;
-
-    while (1) {
-        scanf("%d", &numAdd);
-
-        int i = tamanho - 1;
-
-        
-        while (i >= 0 && numAdd < base[i]) {
-            base[i + 1] = base[i];
-            i--;
-        }
-
-        base[i + 1] = numAdd;
-        tamanho++;
-
-        for (int j = 0; j < tamanho; j++) {
-            printf("%d ", base[j]);
-        }
-    }
+	
+    No* raiz = NULL;
+    while (1){
+    	int valor;
+    	raiz = inserir(raiz, 10);
+	    inserir(raiz, 5);
+	    inserir(raiz, 15);
+		printf("\nvalor que deseja adicionar:");
+    	scanf("%d",&valor);
+    	inserir(raiz,valor);
+	    
+	    emOrdem(raiz);
+	}
+	liberar(raiz);
 
     return 0;
 }
